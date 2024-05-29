d3e3378c1ba5e50b9cd69f4c9d218ca1f8ce4b5b
ShripriyaR <shripriyaravi@gmail.com>
2024-05-29 17:31:40 +0530

    first commit

---
+++ src/redux/slice/userSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// const initialStateValue = {};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        user: null,
+        isAuthenticated: false,
+    },
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload;
+            state.isAuthenticated = true;
+            
+        },
+        logout: (state, action) => {
+            state.user = null;
+            state.isAuthenticated = false;
+           
+        },
+        updateUser: (state, action) => {
+            state.user = {
+                ...state.user,
+                email: action.payload.email // Assuming payload contains updated email
+            };
+        }
+    }
+})
+export const {login, logout, updateUser} = userSlice.actions;
+export default userSlice.reducer;
